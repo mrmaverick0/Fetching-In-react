62aac87c8575d19b9e8f70dc6f0409b6f7b02df2
Mohd Umar <ua1411978@gmail.com>
2023-06-24 19:27:53 +0530

    Error proof

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movie, setMovie] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  async function fetchMovies() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+      const data = await response.json();
+
+      const transformMovie = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovie(transformMovie);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovies}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movie.length > 0 && <MoviesList movies={movie} />}
+        {!isLoading && movie.length === 0 && !error && <p>Found no Movies</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
